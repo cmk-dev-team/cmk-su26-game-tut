311678473e64c974fd3417abd3f8873a2c240f46
takunology <takunology.dev@gmail.com>
2026-07-04 01:08:32 +0900

    Prepare MakeCode release v1.0.69

--- custom.ts
+++ custom.ts
@@ -293,7 +293,7 @@ namespace GameSettings {
     //% blockHidden=true
     //% weight=110
     export function extensionVersion(): string {
-        return "1.0.68"
+        return "1.0.69"
     }
 }
 
