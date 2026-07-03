b4dd5bd038c1ea12e9449d084e7366898a39f977
takunology <takunology.dev@gmail.com>
2026-07-03 23:45:56 +0900

    Restore team puzzle blocks

--- custom.ts
+++ custom.ts
@@ -293,7 +293,7 @@ namespace GameSettings {
     //% blockHidden=true
     //% weight=110
     export function extensionVersion(): string {
-        return "1.0.64"
+        return "1.0.65"
     }
 }
 
